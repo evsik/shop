6c297d4e8ea9d478d0d516474415b352e02c9155
evsik <evsevsikov@gmail.com>
2021-02-01 18:43:36 +0300

    add items comp

--- src/components/items/items.js
+++ src/components/items/items.js
@@ -1,2 +1,14 @@
 import Item from '../item/item';
 
+export class BasketItem extends Item {
+    constructor(item) {
+        super(item);
+        this.amount = 1;
+    }
+
+    render() {
+        //
+    }
+}
+
+export class CatalogItem extends Item { }
