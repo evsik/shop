e0ed0f1b2b7b6c43a039fe0b74e508b67d6db32e
evsik <evsevsikov@gmail.com>
2021-02-09 12:51:52 +0300

    added catalog vue comp

---
+++ src/components/catalog.vue
@@ -0,0 +1,34 @@
+<template>
+
+  <div class="products container">
+    <item
+        v-for="item of items"
+        :key="item.id_product"
+        :item="item"
+    />
+  </div>
+
+</template>
+
+<script>
+import item from './item.vue';
+import { get } from '../utils/reqs.js';
+
+export default {
+  components: { item },
+  data() {
+    return {
+      url: 'https://raw.githubusercontent.com/evsik/static/master/JSON%20files/catalog.json',
+      items: []
+    }
+  },
+  mounted() {
+    get(this.url)
+        .then(data => { this.items = data })
+  }
+}
+</script>
+
+<style>
+
+</style>
